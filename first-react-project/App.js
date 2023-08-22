f7972647ecb02246d15319db0fd72c613bda26d8
Darth Vader <danieltarancon@gmail.com>
2023-08-22 18:25:23 +0200

    22 1825

--- first-react-project/App.js
+++ first-react-project/App.js
@@ -5,6 +5,10 @@ import './css/index.css';
 import SearchForm from './SearchForm';
 import MainNav from './MainNav';
 import PhotoContainer from './PhotoContainer';
+import apiKey from './config';
+
+const apiKey = apiKey;
+
 
 
 function App() {
