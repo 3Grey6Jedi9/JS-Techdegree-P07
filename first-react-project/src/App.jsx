d0d0374b676eff16cbee202c8d8842730e610c43
Darth Vader <danieltarancon@gmail.com>
2023-08-27 22:40:47 +0200

    27 2240

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import SearchForm from "./components/SearchForm.jsx";
 import Nav from "./components/Nav.jsx";
 import PhotoContainer from "./components/PhotoContainer.jsx";
@@ -11,9 +11,34 @@ import Home from "./components/Home.jsx";
 import Computers from "./components/Computers.jsx";
 import SearchResults from "./components/SearchResults.jsx";
 import NotFound from "./components/NotFound.jsx";
+import axios from "axios";
+
+
+let tags = "sunsets";
+let perPage = 24;
+const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&per_page=${perPage}&format=json&nojsoncallback=1`;
+
 
 
 const App = () => {
+
+    useEffect(() => {
+
+        // Fetching data here using axios
+        axios.get(apiUrl) // Sending a GET request
+            .then(response => {
+                // This block of code will run when the request is succesful
+                console.log(response.data); // Accessing the parsed JSON data
+
+            })
+            .catch(error => {
+                // This block of code runs when there's an error
+                console.error('Error fetching data:', error);
+
+            });
+
+    }, []); // Empty dependency array ensures this runs once when the component mounts
+
     return (
         <Router>
             <div className="container">
