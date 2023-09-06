c001f4c3d73a002c0b5bfac14de4dde656c0eb1d
Darth Vader <danieltarancon@gmail.com>
2023-09-06 18:23:53 +0200

    06 1823

--- first-react-project/src/components/NotFoundRedirect.jsx
+++ first-react-project/src/components/NotFoundRedirect.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const NotFoundRedirect = () => {
     const navigate = useNavigate();
 
-    // Checking if the current path doesn't match specific paths
-    const currentPath = window.location.pathname;
-    const isInvalidPath = !currentPath.startsWith("/search") &&
-        currentPath !== "/" &&
-        currentPath !== "/cats" &&
-        currentPath !== "/dogs" &&
-        currentPath !== "/computers" &&
-        currentPath !== "/not-found";
+    // Using the useEffect hook to perform the redirect logic once the component is mounted
+    useEffect(() => {
+        // Checking if the current path doesn't match specific paths
+        const currentPath = window.location.pathname;
+        const isInvalidPath = !currentPath.startsWith("/search") &&
+            currentPath !== "/" &&
+            currentPath !== "/cats" &&
+            currentPath !== "/dogs" &&
+            currentPath !== "/computers" &&
+            currentPath !== "/not-found";
 
-    // If it's an invalid path, we'll navigate to the /not-found page
-    if (isInvalidPath) {
-        navigate("/not-found");
-    }
+        // If it's an invalid path, we'll navigate to the /not-found page
+        if (isInvalidPath) {
+            navigate("/not-found");
+        }
+    }, [navigate]); // Every time we navigate I verify conditions
 
-    // I will return null because the goal of this file is to manage the wrong route not to display any message
+    // The file returns null because the goal of this file is to manage the wrong route, not to display any message
     return null;
 };
 
 export default NotFoundRedirect;
+
